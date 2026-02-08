5a091c2ef4575f53f3109d7eff25b4c9ec5ec95e
jesrelqwert <jesrelalvarez@gmail.com>
2026-02-08 13:21:29 -0600

    tabla de costo funcionando

---
+++ dijkstra.py
@@ -0,0 +1,34 @@
+
+
+class Node:
+    def __init__(self, nodename, connections):
+        self.nodename = nodename
+        self.connections = connections
+
+        self.cost_table = {nodename: {'cost':0, 'previous':None}}
+
+        for name, cost in self.connections.items():
+            self.cost_table[name] = {'cost': cost, 'previous': self.nodename}
+
+
+    def __repr__(self):
+        cost_table_header = '\tNode\tCost\tPrevious\n'
+        cost_table_str = ''.join([f'\t{node}\t{vals["cost"]}\t{vals["previous"]}\n' for node, vals in self.cost_table.items()])
+
+        return f'nodename: {self.nodename}\nconnections:{self.connections}\ncost_table:\n {cost_table_header}{cost_table_str}'
+    
+    def __str__(self):
+        cost_table_header = '\tNode\tCost\tPrevious\n'
+        cost_table_str = ''.join([f'\t{node}\t{vals["cost"]}\t{vals["previous"]}\n' for node, vals in self.cost_table.items()])
+
+        return f'nodename: {self.nodename}\nconnections:{self.connections}\ncost_table:\n {cost_table_header}{cost_table_str}'
+
+
+
+
+
+if __name__ == '__main__':
+    # Se definen nodos
+    A = Node('A', {'B':5, 'C':7, 'D':2})
+
+    print(A)
